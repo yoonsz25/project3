f5ad1b82c71fb94b01c0237ee5743ae587c1884c
yoonsz25 <yoonsz25@yahoo.com>
2016-02-28 02:39:03 +0000

    Started AdList. Added functions

--- AdList.cpp
+++ AdList.cpp
@@ -1,2 +1,69 @@
-//this is going to be the Adjacent List.cpp
-//testing.
+#include "AdList.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//constructor: name is automatically = "!"
+AdList::AdList(){
+    TABLE_SIZE = 211;
+    count = 0;
+    arr = new HashEntry[TABLE_SIZE];
+    for(int i = 0; i<TABLE_SIZE;i++){
+        arr[i].name = "!";
+    }
+}
+
+//returns the hash index for a certain string.
+int AdList::hash(string str, int seed=0){
+    int hash = seed;
+    for(int i =0; i<str.length(); i++)
+        hash = hash*101 + str[i];
+    return hash%TABLE_SIZE;
+}
+
+void AdList::insertHash(string name){
+    int index = hash(name);
+    for(int i = index; i < this->TABLE_SIZE; i++){
+        if(arr[i].compare("!") == 0){
+            index = i;
+            break;
+        }
+        if(i == (this->TABLE_SIZE-1)){
+            i = 0;
+        }
+    }
+    
+    arr[index].name = name;
+    arr[index].dataPointer = count;
+    cout << "index is: " << index << ", pointer to data is: " << arr[index].dataPointer << "count is: " << this->count << endl;
+}
+
+void AdList::insertData(string line){
+    ++this->count;
+    string name ="";
+    
+    insertHash(name);
+
+}
+
+//update's "name"'s friends list by adding nameFriend.
+void AdList::addFriend(string name, string nameFriend){
+    int index = hash(name);
+    int i = 0;
+    while(arr[index].name.compare(name)!=0){
+        index = hash(name,i++);
+    }
+    arr[index].friends.push_back(nameFriend);
+}
+
+//friend A adds friend B to A's list and B adds A to B's list.
+void AdList::updateFriend(string a, string b){
+    addFriend(a,b);
+    addFriend(b,a);
+    return;
+}
+
+void AdLis::print(){
+    
+}
--- AdList.h
+++ AdList.h
@@ -1,18 +1,25 @@
 #include <string>
 #include <list>
 
+
 class AdList{
 	private:
 		struct HashEntry{
 			std::string name;
-			int dataLocation;
+			int dataPointer;
 			std::list<std::string> friends;
 		};
+		HashEntry *arr;
 		int count;
+		int TABLE_SIZE;
 
+		void insertHash(std::string name);
 
 	public:
+		AdList();
 		int hash(std::string str, int seed=0);
-		void insert(std::string name);
+		void insertData(std::string line);
 		void addFriend(std::string name, std::string nameFriend); //One friend
+		void updateFriend(std::string a, std::string b) //updates both friends' list
+		void print();
 }
