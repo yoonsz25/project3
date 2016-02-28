7bbc27e5f23f5f53f15d972dd50071305279b66a
Alex <Alex@ResNet-63-201.resnet.ucsb.edu>
2016-02-27 17:39:19 -0800

    initial

--- AdList.h
+++ AdList.h
@@ -1,1 +1,18 @@
-//this is going to be the header file.
+#include <string>
+#include <list>
+
+class AdList{
+	private:
+		struct HashEntry{
+			std::string name;
+			int dataLocation;
+			std::list<std::string> friends;
+		};
+		int count;
+
+
+	public:
+		int hash(std::string str, int seed=0);
+		void insert(std::string name);
+		void addFriend(std::string name, std::string nameFriend); //One friend
+}
