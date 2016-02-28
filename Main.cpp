0eb241527ff6b108ec0fa690e06b5c238ee243fa
Yoon <yoonlee@umail.ucsb.edu>
2016-02-27 17:28:46 -0800

    tests and main

---
+++ Main.cpp
@@ -0,0 +1,43 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: Feb 11, 2016
+ *      Author: omid
+ */
+
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<string> split(string str, char delimiter)
+{
+	vector<string> internal;
+	stringstream ss(str);       // turn the string into a stream.
+	string tok;
+
+	while(getline(ss, tok, delimiter))
+	{
+		internal.push_back(tok);
+	}
+
+	return internal;
+}
+
+int main()
+{
+	ifstream f;
+	f.open("/home/omid/PythonProjects/Generated1.txt", ios::in);
+	if(!f) cerr << "File not found" << endl;
+	else
+	{
+		string line;
+		while(std::getline(f, line))
+		{
+			vector<string> words = split(line, ',');
+			// ... TO DO ...
+		}
+	}
+	return 0;
+}
